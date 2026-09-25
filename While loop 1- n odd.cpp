1d35516d48438682a7d4df3d21b90361a851f529
Anik-Banerjee-ui <anikban0@gmail.com>
2026-09-25 16:25:46 +0530

    Add files via upload

---
+++ While loop 1- n odd.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,a=1;
+	printf("ENter a Number:\n");
+	scanf("%d",&n);
+	while(a<=n)
+	{
+		printf("\nThe number is:%d",a);
+		a+=2;
+	}
+	
+	
+	
+	
+	return 0;
+}
